938545ae6afd8d3750d1fed94ba796583ed4641c
boltac-tp <boltac.tp@gmail.com>
2020-06-02 18:41:38 +0900

    fix:install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-ln -s /dotfiles/nvim ~/.config/nvim
-ln -s /dotfiles/.gitcofig ~/.gitconfig
-ln -s /dotfiles/.zshrc ~/.zshrc
-ln -s /dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/.gitcofig ~/.gitconfig
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
