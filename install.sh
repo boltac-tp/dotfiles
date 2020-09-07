b4b3ccfe254f9f8df1f2f789839188a6a9aac84b
boltac-tp <boltac.tp@gmail.com>
2020-09-08 01:23:47 +0900

    upd:install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!bin/bash
 
+sudo apt update
+sudo apt install zsh
+chsh -s $(which zsh)
+
+sudo apt update
+sudo apt install vim
+
+sudo apt update
+sudo apt install neovim
+
+sudo add-apt-repository ppa:gitcore/ppa
+sudo apt update
+sudo apt install git
+
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.zshrc ~/.zshrc
