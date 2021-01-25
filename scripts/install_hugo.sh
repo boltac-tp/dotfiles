e13d32bbc7bcaeac4d65bfb82a2e2914a45bd82b
boltac-tp <boltac.tp@gmail.com>
2021-01-25 21:08:22 +0900

    upd : refactor install_hugo.sh

--- scripts/install_hugo.sh
+++ scripts/install_hugo.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
+VERSION=0.78.2
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.78.2/hugo_extended_0.78.2_Linux-64bit.deb
-sudo apt install ./hugo_extended_0.78.2_Linux-64bit.deb
+wget https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_extended_0.78.2_Linux-64bit.deb
+sudo apt install ./hugo_extended_${VERSION}_Linux-64bit.deb
+rm ./hugo_extended_${VERSION}_Linux-64bit.deb
 
-cd
-mkdir site
-cd site
-
-git clone --recursive git@github.com:boltac-tp/boltac.git
-git clone --recursive git@github.com:boltac-tp/debit.git
+if [ ! -d ~/site ]; then
+  cd
+  mkdir site
+  cd site
+  git clone --recursive git@github.com:boltac-tp/boltac.git
+  git clone --recursive git@github.com:boltac-tp/debit.git
+fi
