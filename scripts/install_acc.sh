0fa7cb29cc6fcbeac30e290527d1ab9e003bd64e
boltac-tp <boltac.tp@gmail.com>
2021-02-24 19:42:04 +0900

    add : cargo-compete

--- scripts/install_acc.sh
+++ scripts/install_acc.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+# install atcoder - cli
 pip3 install --user online-judge-tools
 npm install -g atcoder-cli
 
@@ -7,3 +8,8 @@ mkdir -p ~/.config/atcoder-cli-nodejs
 
 ln -s ~/dotfiles/atcoder-cli-nodejs/python3 ~/.config/atcoder-cli-nodejs/python3
 ln -s ~/dotfiles/atcoder-cli-nodejs/ts ~/.config/atcoder-cli-nodejs/ts
+
+#install cargo-compete
+sudo apt install pkg-check
+cargo install cargo-compete
+
