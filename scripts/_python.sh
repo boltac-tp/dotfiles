e40689d92da366354a96f96bdca841fae532f31b
boltac-tp <boltac.tp@gmail.com>
2021-07-12 16:35:50 +0900

    fix : _python.sh poetry condif

--- scripts/_python.sh
+++ scripts/_python.sh
@@ -5,7 +5,7 @@ echo '\n install Python & utils \n \n'
 sudo apt install -y python3-venv python3-pip
 # install poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python3 - 
-poetry config virtualenvs.in-project=true
+poetry config virtualenvs.in-project true
 # install pyenv
 sudo apt install -y libffi-dev libssl-dev libsqlite3-dev libbz2-dev libreadline-dev
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
