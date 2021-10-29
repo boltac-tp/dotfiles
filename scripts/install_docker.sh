fb40591f1a522113778133be938ccb925297dc49
boltac-tp <boltac.tp@gmail.com>
2021-10-29 23:54:43 +0900

    upd: ubuntu install

---
+++ scripts/install_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker ${USER}
+
+# 最新バージョン番号の取得
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
+
+# 最新バージョンのダウンロード
+sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+# 実行可能モードを設定
+sudo chmod +x /usr/local/bin/docker-compose
