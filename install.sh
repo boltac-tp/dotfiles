28bea4031a35739042c00bce069419958e625a61
boltac-tp <boltac.tp@gmail.com>
2021-12-03 08:53:40 +0900

    upd: install.sh add vscode

--- install.sh
+++ install.sh
@@ -23,6 +23,16 @@ if [[ $MY_ENV = GNOME ]] || [[ $MY_ENV = KDE ]]; then
   sudo apt update
   sudo apt install -y solaar
 
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm -f packages.microsoft.gpg
+
+  sudo apt install -y apt-transport-https
+  sudo apt update
+  sudo apt install -y code
+
+
   if [[ $MY_ENV = GNOME ]]; then
     # script for Ubuntu
     LANG=C xdg-user-dirs-gtk-update
