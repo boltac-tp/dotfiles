6640dba937e462906e8276af1c41339adb7309cb
boltac-tp <boltac.tp@gmail.com>
2022-09-07 01:10:37 +0900

    upd: scripts install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -5,15 +5,9 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-sudo usermod -aG docker ${USER}
-
-# 最新バージョン番号の取得
-COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
-
-# 最新バージョンのダウンロード
-sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo usermod -aG docker "${USER}"
 
 # 実行可能モードを設定
 sudo chmod +x /usr/local/bin/docker-compose
