67cedca5904b347ea03121edf2a8db5423fe5cbd
boltac-tp <boltac.tp@gmail.com>
2023-07-30 09:53:16 +0900

    update: change keymaps for nvim-lsp

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -21,43 +21,44 @@ s("n", "<leader>fh", telescope_builtin.help_tags, { noremap = true })
 s("n", "<leader>fb", telescope.extensions.file_browser.file_browser, { noremap = true })
 s("n", "<leader>fs", telescope.extensions.luasnip.luasnip, { noremap = true })
 
-
--- lsp
---s("n", "<space>d", vim.diagnostic.open_float , { noremap = true, silent = true })
---s("n", "[d", vim.diagnostic.goto_prev, { noremap = true, silent = true })
---s("n", "]d", vim.diagnostic.goto_next, { noremap = true, silent = true })
-s("n", "<leader>q", vim.diagnostic.setloclist, { noremap = true, silent = true })
+-- lsp Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+--s("n", "<space>d", vim.diagnostic.open_float)
+--s("n", "[d", vim.diagnostic.goto_prev)
+--s("n", "]d", vim.diagnostic.goto_next)
+s("n", "<space>q", vim.diagnostic.setloclist)
 
 -- lspsaga
 -- lsp finder
-s("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+s("n", "gh", "<cmd>Lspsaga finder<CR>", { silent = true })
 -- code action
 s("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
 -- rename
 s("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { silent = true })
 -- Definition preview
 s("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
-s("n", "gd", "<cmd>Lspsaga goto_difinition<CR>", { silent = true })
--- Show diagonostics
-s("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
-s("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
--- Diagnsotic jump can use '<c-o>' to jump back
-s("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
-s("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
--- Only jump to error
-s("n", "[E", function()
-    require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
-end, { silent = true })
-s("n", "]E", function()
-    require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
-end, { silent = true })
+s("n", "gD", "<cmd>Lspsaga goto_definition<CR>", { silent = true })
+-- Definition type preview
+s("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>", { silent = true })
+s("n", "gT", "<cmd>Lspsaga goto_type_definition<CR>", { silent = true })
+-- Diagnostic jump can use '<c-o>' to jump back
+s("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+s("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
+s("n", "]e", function()
+	require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+end)
+s("n", "[e", function()
+	require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end)
+
+-- Diagnnostic
+s("n", "<leader>d", "<cmd>Lspsaga show_buf_diagnostics<CR>", { silent = true })
 -- Outline
-s("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", { silent = true })
+s("n", "<leader>o", "<cmd>Lspsaga outline<CR>", { silent = true })
 -- Hovor Doc
 s("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 -- float terminal
--- s("n", "<A-t>", "<cmd>Lspsaga open_floaterm<CR>", { silent = true })
--- s("t", "<A-t>", [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], { silent = true })
+-- s("n", "<A-t>", "<cmd>Lspsaga term_toggle<CR>", { silent = true })
 
 --buffer line
 s("n", "]b", "<Cmd>BufferLineCycleNext<CR>", {})
