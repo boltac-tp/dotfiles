34117812ca48196d9af110e475c06e8d18106975
boltac-tp <boltac.tp@gmail.com>
2023-08-08 10:34:57 +0900

    fix: fix wezterm.lua for wsl

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local start
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	start = { "wsl.exe", "~", "--distribution", "Ubuntu-22.04", "--exec", "/usr/bin/zsh", "-l" }
+	start = { "wsl.exe", "~", "--distribution", "Ubuntu", "--exec", "/usr/bin/zsh", "-l" }
 else
 	start = { "/usr/bin/zsh", "-l" }
 end
