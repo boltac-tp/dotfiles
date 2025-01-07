b56f310b3ba5218b9dc7ce83b60de0dbdd455b2a
boltac-tp <boltac.tp@gmail.com>
2025-01-07 14:13:12 +0900

    update:neovim: config git sign

--- nvim/lua/config/gitsigns.lua
+++ nvim/lua/config/gitsigns.lua
@@ -1,4 +1,11 @@
 require("gitsigns").setup({
+	signs = {
+		add = { text = "+" },
+		change = { text = "~" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+	},
 	on_attach = function(bufnr)
 		local gitsigns = require("gitsigns")
 
@@ -15,7 +22,7 @@ require("gitsigns").setup({
 			else
 				gitsigns.nav_hunk("next")
 			end
-		end)
+		end, { desc = "Gitsigns next hunk" })
 
 		map("n", "[c", function()
 			if vim.wo.diff then
@@ -23,32 +30,32 @@ require("gitsigns").setup({
 			else
 				gitsigns.nav_hunk("prev")
 			end
-		end)
+		end, { desc = "Gitsigns prev hunk" })
 
 		-- Actions
-		map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "gitsigns stage hunk" })
-		map("n", "<leader>gr", gitsigns.reset_hunk)
-		map("v", "<leader>gs", function()
+		map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Gitsigns stage hunk" })
+		map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Gitsigns reset hunk" })
+		map("v", "<leader>hS", function()
 			gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-		end)
-		map("v", "<leader>gr", function()
+		end, { desc = "Gitsigns stage hunk" })
+		map("v", "<leader>hR", function()
 			gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-		end)
-		map("n", "<leader>gS", gitsigns.stage_buffer)
-		map("n", "<leader>gu", gitsigns.undo_stage_hunk)
-		map("n", "<leader>gR", gitsigns.reset_buffer)
-		map("n", "<leader>gp", gitsigns.preview_hunk)
-		map("n", "<leader>gb", function()
+		end, { desc = "Gitsigns reset hunk" })
+		map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Gitsigns stage buffer" })
+		map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "Gitsigns undo stage hunk" })
+		map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Gitsigns reset buffer" })
+		map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Gitsigns preview hunk" })
+		map("n", "<leader>hb", function()
 			gitsigns.blame_line({ full = true })
-		end)
-		map("n", "<leader>gb", gitsigns.toggle_current_line_blame)
-		map("n", "<leader>gd", gitsigns.diffthis)
-		map("n", "<leader>gD", function()
+		end, { desc = "Gitsigns blame line" })
+		map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Gitsigns toggle current line blame" })
+		map("n", "<leader>hd", gitsigns.diffthis, { desc = "Gitsigns diffthis" })
+		map("n", "<leader>hD", function()
 			gitsigns.diffthis("~")
-		end)
-		map("n", "<leader>ggd", gitsigns.toggle_deleted)
+		end, { desc = "Gitsigns diffthis" })
+		map("n", "<leader>td", gitsigns.toggle_deleted, { desc = "Gitsigns toggle deleted" })
 
 		-- Text object
-		map({ "o", "x" }, "gh", ":<C-U>Gitsigns select_hunk<CR>")
+		map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Gitsigns select hunk" })
 	end,
 })
--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -13,6 +13,12 @@ s("n", "j", "gj", { noremap = true, silent = true })
 s("n", "k", "gk", { noremap = true, silent = true })
 s("n", "ge", "G", { noremap = true, silent = true })
 
+--  See `:help wincmd` for a list of all window commands
+s("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+s("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+s("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+s("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
 -- ctrl-jj to ESC
 s("i", "<C-j><C-j>", "<ESC>", { noremap = true, silent = true })
 
@@ -21,11 +27,11 @@ s("n", "<Esc><Esc>", "<cmd>nohlsearch<CR><Esc>", { noremap = true, silent = true
 
 -- caps
 s("i", "<C-l>", function()
-    local line = vim.fn.getline(".")
-    local col = vim.fn.getpos(".")[3]
-    local substring = line:sub(1, col - 1)
-    local result = vim.fn.matchstr(substring, [[\v<(\k(<)@!)*$]])
-    return "<C-w>" .. result:upper()
+	local line = vim.fn.getline(".")
+	local col = vim.fn.getpos(".")[3]
+	local substring = line:sub(1, col - 1)
+	local result = vim.fn.matchstr(substring, [[\v<(\k(<)@!)*$]])
+	return "<C-w>" .. result:upper()
 end, { expr = true })
 --
 -- telescope
@@ -40,10 +46,10 @@ s("n", "<leader>fd", telescope_builtin.diagnostics, { noremap = true, desc = "te
 s("n", "<leader>fk", telescope_builtin.keymaps, { noremap = true, desc = "telescope keymaps" })
 s("n", "<leader>fc", telescope_builtin.commands, { noremap = true, desc = "telescope command pallete" })
 s(
-    "n",
-    "<leader>fe",
-    telescope.extensions.file_browser.file_browser,
-    { noremap = true, desc = "telescope file browser" }
+	"n",
+	"<leader>fe",
+	telescope.extensions.file_browser.file_browser,
+	{ noremap = true, desc = "telescope file browser" }
 )
 s("n", "<leader>fs", telescope.extensions.luasnip.luasnip, { noremap = true, desc = "telescope luasnip snipets" })
 
@@ -52,13 +58,14 @@ s("n", "<leader>fs", telescope.extensions.luasnip.luasnip, { noremap = true, des
 s("n", "<leader>dd", vim.diagnostic.open_float, { silent = true, desc = "diagnostic open float" })
 
 s("n", "[d", function()
-    vim.diagnostic.jump({ count = 1, float = true })
+	vim.diagnostic.jump({ count = 1, float = true })
 end, { silent = true, desc = "diagnostic jump to next" })
 
 s("n", "]d", function()
-    vim.diagnostic.jump({ count = -1, float = true })
+	vim.diagnostic.jump({ count = -1, float = true })
 end, { silent = true, desc = "diagnostic jump to prev" })
-s("n", "<leader>q", vim.diagnostic.setloclist)
+
+s("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic quickfix list" })
 
 --buffer line
 s("n", "]b", "<Cmd>BufferLineCycleNext<CR>", { silent = true, desc = "BufferLine cycle next" })
@@ -71,43 +78,43 @@ s("n", "<leader>9", ":<C-u>CodyToggle<CR>", { silent = true, desc = "Cody toggle
 s("n", "<leader>dap", ':lua require("dapui").toggle()<CR>', { desc = "DAP" })
 
 s("n", "<F5>", function()
-    require("dap").continue()
+	require("dap").continue()
 end, { desc = "DAP continue" })
 s("n", "<F10>", function()
-    require("dap").step_over()
+	require("dap").step_over()
 end, { desc = "DAP step over" })
 s("n", "<F11>", function()
-    require("dap").step_into()
+	require("dap").step_into()
 end, { desc = "DAP step into" })
 s("n", "<F12>", function()
-    require("dap").step_out()
+	require("dap").step_out()
 end, { desc = "DAP step out" })
 s("n", "<Leader>m", function()
-    require("dap").toggle_breakpoint()
+	require("dap").toggle_breakpoint()
 end, { desc = "DAP toggle breakpoint" })
 s("n", "<Leader>M", function()
-    require("dap").set_breakpoint()
+	require("dap").set_breakpoint()
 end, { desc = "DAP set breakpoint" })
 s("n", "<Leader>lp", function()
-    require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+	require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
 end, { desc = "DAP log point message" })
 s("n", "<Leader>dr", function()
-    require("dap").repl.open()
+	require("dap").repl.open()
 end, { desc = "DAP repl" })
 s("n", "<Leader>dl", function()
-    require("dap").run_last()
+	require("dap").run_last()
 end, { desc = "DAP run last" })
 s({ "n", "v" }, "<Leader>dh", function()
-    require("dap.ui.widgets").hover()
+	require("dap.ui.widgets").hover()
 end, { desc = "DAP hover" })
 s({ "n", "v" }, "<Leader>dp", function()
-    require("dap.ui.widgets").preview()
+	require("dap.ui.widgets").preview()
 end, { desc = "DAP preview" })
 s("n", "<Leader>df", function()
-    local widgets = require("dap.ui.widgets")
-    widgets.centered_float(widgets.frames)
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.frames)
 end, { desc = "DAP centered float frames" })
 s("n", "<Leader>ds", function()
-    local widgets = require("dap.ui.widgets")
-    widgets.centered_float(widgets.scopes)
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.scopes)
 end, { desc = "DAP centered float scopes" })
