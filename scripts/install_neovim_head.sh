be6334053d0fa6907288fd46ab420182aba2d6b5
boltac-tp <boltac.tp@gmail.com>
2025-01-30 23:44:36 +0900

    fix:scripts: install script of nvim

--- scripts/install_neovim_head.sh
+++ scripts/install_neovim_head.sh
@@ -1,9 +1,9 @@
-wget -q https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-tar xzf nvim-linux64.tar.gz -C ~/.local/share/
-rm nvim-linux64.tar.gz
+wget -q https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
+tar xzf nvim-linux-x86_64.tar.gz -C ~/.local/share/
+rm nvim-linux-x86_64.tar.gz
 
 if ! [ -e ~/.local/bin/nvim ]; then
-	ln -s ~/.local/share/nvim-linux64/bin/nvim ~/.local/bin/nvim
+    ln -s ~/.local/share/nvim-linux-x86_64/bin/nvim ~/.local/bin/nvim
 fi
 
 nvim --version
