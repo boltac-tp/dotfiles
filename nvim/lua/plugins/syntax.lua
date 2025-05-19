b40377ebddb9803f232b434e554e4d698335b0b0
boltac-tp <boltac.tp@gmail.com>
2025-05-19 22:53:06 +0900

    fix:nvim: nvim-treeの設定ができていなかった

--- nvim/lua/plugins/syntax.lua
+++ nvim/lua/plugins/syntax.lua
@@ -3,13 +3,16 @@ return {
 	build = ":TSUpdate",
 	event = { "BufRead", "InsertEnter" },
 	dependencies = { "yioneko/nvim-yati" },
-	opts = {
-		ensure_installed = "all",
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_hilighting = false,
-		},
-		yati = { enable = true },
-	},
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = "all",
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_hilighting = false,
+			},
+			yati = { enable = true },
+		})
+	end,
 }
